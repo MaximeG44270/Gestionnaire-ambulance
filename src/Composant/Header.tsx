9b908b55898060b456af6ab412a6965a2cc0bc4b
MaximeG44270 <gilbert.maxime01@gmail.com>
2025-01-19 13:37:23 +0100

    Ajout multiple
    Multiple ajout sur Slidebar.tsx et header

--- src/Composant/Header.tsx
+++ src/Composant/Header.tsx
@@ -1,67 +1,100 @@
 import React, { useEffect, useState } from "react";
 import { getDoc, doc } from "firebase/firestore";
-import { db } from "../firebaseConfig";
+import { signOut } from "firebase/auth";
+import { auth, db } from "../firebaseConfig";
+import { useNavigate } from "react-router-dom";
+import Slidebar from "./Slidebar";
 import "../index.css";
 import "../fonts.css";
-import Slidebar from "./Slidebar";
+
 
 const Header: React.FC = () => {
   const [firstName, setFirstName] = useState<string | null>("Nom Utilisateur");
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   const fetchFirstName = async () => {
     try {
-      const userId = "ilfDFGYO6LYOCPu7GGfIBX0MoYF3";
-      const userDocRef = doc(db, "users", userId);
-      const userDoc = await getDoc(userDocRef);
+      const userId = auth.currentUser?.uid;
+      if (userId) {
+        const userDocRef = doc(db, "users", userId);
+        const userDoc = await getDoc(userDocRef);
 
-      if (userDoc.exists()) {
-        const userData = userDoc.data();
-        setFirstName(userData.firstName);
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          setFirstName(userData.firstName);
+        } else {
+          console.error("Aucun document trouvé pour cet utilisateur.");
+        }
       } else {
-        console.error("Aucun document trouvé pour cet utilisateur.");
+        console.error("Aucun utilisateur connecté.");
       }
     } catch (error) {
       console.error("Erreur lors de la récupération des données utilisateur :", error);
     }
   };
 
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/auth/login");
+    } catch (error) {
+      console.error("Erreur lors de la déconnexion :", error);
+    }
+  };
+
   useEffect(() => {
     fetchFirstName();
   }, []);
-  return (
-      <div className="w-full p-4 bg-white shadow-md">
-        <div className="flex justify-between items-center">
-          <div className="w-1/3 flex items-center">
-            <img src="/image/logo-amb.png" alt="Logo" className="w-16 h-12 mr-5" />
-            <p className="text-base FC-BM hidden custom3:block font-carving-semi-bold">Ambulance Digital</p>
-          </div>
-          <div className="w-2/3 flex justify-end items-center">
-            <input
-              type="text"
-              placeholder="Rechercher..."
-              className="px-4 py-2 border rounded-lg focus:outline-none hidden custom:block focus:ring-2 focus:ring-blue-500 font-carving-semi-bold mr-20"
-            />
-            <p className="text-base FC-BM font-carving-semi-bold mr-20">{firstName || "Nom Utilisateur"}</p>
-          </div>
-{/* ----------------------------------------------- MENU BRUGER ----------------------------------------------- */}
-          <div
-            className={`burger custom2:hidden ${isMobileMenuOpen ? "open" : ""}`}
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-          >
-            <div className="burger-line line-1"></div>
-            <div className="burger-line line-2"></div>
-            <div className="burger-line line-3"></div>
-          </div>
-        </div>
 
-{/* ----------------------------------------------- MOBILE MENU ----------------------------------------------- */}
-        {isMobileMenuOpen && (
-          <div className="custom2:hidden scrollbar-custom mobile-menu bg-white">
+  return (
+    <div className="w-full p-4 bg-white shadow-md">
+      <div className="flex justify-between items-center">
+        <div className="w-1/3 flex items-center">
+          <img src="/image/logo-amb.png" alt="Logo" className="w-16 h-12 mr-5" />
+          <p className="text-base FC-BM hidden custom3:block font-carving-semi-bold">
+            Ambulance Digital
+          </p>
+        </div>
+        <div className="w-2/3 flex justify-end items-center">
+        {/* TODO: faire peut-etre une barre de recherche mais pas encore l'idée
+          <input
+            type="text"
+            placeholder="Rechercher..."
+            className="px-4 py-2 border rounded-lg focus:outline-none hidden custom:block focus:ring-2 focus:ring-blue-500 font-carving-semi-bold mr-20"
+          /> */}
+          <p className="text-base FC-BM font-carving-semi-bold mr-6">
+            {firstName || "Nom Utilisateur"}
+          </p>
+          <img
+            src="/image/deco.png"
+            alt="Déconnexion"
+            className="w-6 h-6 mr-8 cursor-pointer"
+            onClick={handleLogout}
+          />
+        </div>
+        <div
+          className={`burger custom2:hidden ${isMobileMenuOpen ? "open" : ""}`}
+          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+        >
+          <div className="burger-line line-1"></div>
+          <div className="burger-line line-2"></div>
+          <div className="burger-line line-3"></div>
+        </div>
+      </div>
+
+      {isMobileMenuOpen && (
+        <>
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 z-40"
+            onClick={() => setIsMobileMenuOpen(false)}
+          ></div>
+          <div className="fixed inset-y-0 left-0 z-50">
             <Slidebar />
           </div>
-        )}
-      </div>
+        </>
+      )}
+    </div>
   );
 };
 
--- src/Composant/Slidebar.tsx
+++ src/Composant/Slidebar.tsx
@@ -1,19 +1,28 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../index.css";
 import "../fonts.css";
 
 const Slidebar: React.FC = () => {
   const [isUserOpen, setIsUserOpen] = useState(false);
   const [isDesinfectionOpen, setIsDesinfectionOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname.startsWith("/utilisateurs")) {
+      setIsUserOpen(true);
+    } else if (location.pathname.startsWith("/resultats-desinfection")) {
+      setIsDesinfectionOpen(true);
+    }
+  }, [location.pathname]);
 
   return (
-    <div className="w-64 h-2/3 bg-white">
+    <div className="w-64 xl:h-2/3 h-screen bg-white">
       <div className="p-4 text-xl font-carving-black">Tableau de bord</div>
       <ul className="space-y-2">
-{/* ----------------------------------------------- ACCUEIL ----------------------------------------------- */}
+        {/* ----------------------------------------------- ACCUEIL ----------------------------------------------- */}
         <li className="p-2 flex items-center gap-3 font-carving-black">
-          <div className=" flex items-center ml-1 gap-3 w-72 rounded-md  hover:bg-[#0f6ca5] hover:text-white">
+          <div className={`flex items-center ml-1 gap-3 w-72 rounded-md ${location.pathname === "/dashboard" ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white`}>
             <img src="/image/icone-acceuil.png" alt="Logo" className="w-6 h-4" />
             <Link to="/dashboard" className="h-10 flex items-center w-full">
               Acceuil
@@ -21,9 +30,9 @@ const Slidebar: React.FC = () => {
           </div>
         </li>
 
-{/* ----------------------------------------------- PHARMACIE INTERNE ----------------------------------------------- */}
+        {/* ----------------------------------------------- PHARMACIE INTERNE ----------------------------------------------- */}
         <li className="p-2 flex items-center gap-3 font-carving-black">
-          <div className=" flex items-center gap-3 w-72 ml-1 rounded-md  hover:bg-[#0f6ca5] hover:text-white">
+          <div className={`flex items-center gap-3 w-72 ml-1 rounded-md ${location.pathname === "/pharmacie-interne" ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white`}>
             <img src="/image/icone-pharmacie.png" alt="Logo" className="w-6 h-6" />
             <Link to="/pharmacie-interne" className="h-10 flex items-center w-full">
               Pharmacie interne
@@ -31,9 +40,9 @@ const Slidebar: React.FC = () => {
           </div>
         </li>
 
-{/* ----------------------------------------------- AMBULANCE ----------------------------------------------- */}
+        {/* ----------------------------------------------- AMBULANCE ----------------------------------------------- */}
         <li className="p-2 flex items-center gap-3 font-carving-black">
-          <div className=" flex items-center gap-3 w-72 ml-1 rounded-md  hover:bg-[#0f6ca5] hover:text-white">
+          <div className={`flex items-center gap-3 w-72 ml-1 rounded-md ${location.pathname === "/ambulance" ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white`}>
             <img src="/image/fleche-ambulance.png" alt="Logo" className="w-6 h-6" />
             <Link to="/ambulance" className="h-10 flex items-center w-full">
               Ambulance
@@ -41,9 +50,9 @@ const Slidebar: React.FC = () => {
           </div>
         </li>
 
-{/* ----------------------------------------------- VSL & TAXIS ----------------------------------------------- */}
+        {/* ----------------------------------------------- VSL & TAXIS ----------------------------------------------- */}
         <li className="p-2 flex items-center gap-3 font-carving-black">
-          <div className=" flex items-center gap-3 w-72 ml-1 rounded-md  hover:bg-[#0f6ca5] hover:text-white">
+          <div className={`flex items-center gap-3 w-72 ml-1 rounded-md ${location.pathname === "/vsltaxis" ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white`}>
             <img src="/image/icone-vsl-taxis.png" alt="Logo" className="w-6 h-6" />
             <Link to="/vsltaxis" className="h-10 flex items-center w-full">
               VSL/Taxis
@@ -51,10 +60,10 @@ const Slidebar: React.FC = () => {
           </div>
         </li>
 
-{/* ----------------------------------------------- UTILISATEURS ----------------------------------------------- */}
+        {/* ----------------------------------------------- UTILISATEURS ----------------------------------------------- */}
         <li className="p-2">
           <button
-            className="flex items-center justify-between h-10 rounded-md hover:bg-[#0f6ca5] hover:text-white cursor-pointer font-carving-black"
+            className={`flex items-center justify-between h-10 rounded-md ${isUserOpen ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white cursor-pointer font-carving-black`}
             onClick={() => setIsUserOpen(!isUserOpen)}>
             <div className="flex ml-1 items-center gap-3">
               <img src="/image/icone-User.png" alt="Logo" className="w-6 h-6" />
@@ -68,12 +77,12 @@ const Slidebar: React.FC = () => {
           </button>
           {isUserOpen && (
             <ul className="ml-11 mt-2 space-y-2">
-              <li className="hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2">
+              <li className={`hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2 ${location.pathname === "/utilisateurs/liste" ? "bg-[#0f6ca5] text-white" : ""}`}>
                 <Link to="/utilisateurs/liste" className="block w-full h-full">
                   Listes Utilisateurs
                 </Link>
               </li>
-              <li className="hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2">
+              <li className={`hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2 ${location.pathname === "/utilisateurs/ajouter" ? "bg-[#0f6ca5] text-white" : ""}`}>
                 <Link to="/utilisateurs/ajouter" className="block w-full h-full">
                   Ajouter Utilisateurs
                 </Link>
@@ -82,10 +91,10 @@ const Slidebar: React.FC = () => {
           )}
         </li>
 
-{/* ----------------------------------------------- RESULTATS DESINFECTION ----------------------------------------------- */}
+        {/* ----------------------------------------------- RESULTATS DESINFECTION ----------------------------------------------- */}
         <li className="p-2">
           <button
-            className="flex items-center justify-between h-10 rounded-md hover:bg-[#0f6ca5] hover:text-white cursor-pointer font-carving-black"
+            className={`flex items-center justify-between h-10 rounded-md ${isDesinfectionOpen ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white cursor-pointer font-carving-black`}
             onClick={() => setIsDesinfectionOpen(!isDesinfectionOpen)}>
             <div className="flex ml-1 items-center gap-3">
               <img src="/image/icone-Resultat.png" alt="Logo" className="w-6 h-6" />
@@ -99,17 +108,17 @@ const Slidebar: React.FC = () => {
           </button>
           {isDesinfectionOpen && (
             <ul className="ml-11 mt-2 space-y-2">
-              <li className="hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2">
+              <li className={`hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2 ${location.pathname === "/resultats-desinfection/ambulance" ? "bg-[#0f6ca5] text-white" : ""}`}>
                 <Link to="/resultats-desinfection/ambulance" className="block w-full h-full">
                   Ambulance
                 </Link>
               </li>
-              <li className="hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2">
+              <li className={`hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2 ${location.pathname === "/resultats-desinfection/vsl" ? "bg-[#0f6ca5] text-white" : ""}`}>
                 <Link to="/resultats-desinfection/vsl" className="block w-full h-full">
                   Vsl
                 </Link>
               </li>
-              <li className="hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2">
+              <li className={`hover:bg-[#0f6ca5] hover:text-white rounded-md font-carving-semi-bold p-2 ${location.pathname === "/resultats-desinfection/taxis" ? "bg-[#0f6ca5] text-white" : ""}`}>
                 <Link to="/resultats-desinfection/taxis" className="block w-full h-full">
                   Taxis
                 </Link>
@@ -118,9 +127,9 @@ const Slidebar: React.FC = () => {
           )}
         </li>
 
-{/* ----------------------------------------------- PARAMETRES ----------------------------------------------- */}
+        {/* ----------------------------------------------- PARAMETRES ----------------------------------------------- */}
         <li className="p-2 flex items-center gap-3 font-carving-black ">
-          <div className=" flex ml-1 items-center gap-3 w-72 rounded-md  hover:bg-[#0f6ca5] hover:text-white">
+          <div className={`flex ml-1 items-center gap-3 w-72 rounded-md ${location.pathname === "/parametres" ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white`}>
             <img src="/image/icone-setting.png" alt="Logo" className="w-6 h-6" />
             <Link to="/parametres" className="h-10 flex items-center w-full">
               Paramètres
@@ -128,18 +137,19 @@ const Slidebar: React.FC = () => {
           </div>
         </li>
 
-{/* ----------------------------------------------- AJOUT BDD PRODUIT ----------------------------------------------- */}
+        {/* ----------------------------------------------- AJOUT BDD PRODUIT ----------------------------------------------- */}
         <li className="p-2 flex items-center gap-3 font-carving-black">
-          <div className=" flex items-center gap-3 w-72 ml-1 rounded-md  hover:bg-[#0f6ca5] hover:text-white">
+          <div className={`flex items-center gap-3 w-72 ml-1 rounded-md ${location.pathname === "/ajoutBDD" ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white`}>
             <img src="/image/icone-vsl-taxis.png" alt="Logo" className="w-6 h-6" />
             <Link to="/ajoutBDD" className="h-10 flex items-center w-full">
               Ajout produit BDD
             </Link>
           </div>
         </li>
-{/* ----------------------------------------------- AJOUT BDD AMBULANCE ----------------------------------------------- */}
+
+        {/* ----------------------------------------------- AJOUT BDD AMBULANCE ----------------------------------------------- */}
         <li className="p-2 flex items-center gap-3 font-carving-black">
-          <div className=" flex items-center gap-3 w-72 ml-1 rounded-md  hover:bg-[#0f6ca5] hover:text-white">
+          <div className={`flex items-center gap-3 w-72 ml-1 rounded-md ${location.pathname === "/ajoutBDDAmbulance" ? "bg-[#0f6ca5] text-white" : ""} hover:bg-[#0f6ca5] hover:text-white`}>
             <img src="/image/icone-vsl-taxis.png" alt="Logo" className="w-6 h-6" />
             <Link to="/ajoutBDDAmbulance" className="h-10 flex items-center w-full">
               Ajout Ambulance BDD
